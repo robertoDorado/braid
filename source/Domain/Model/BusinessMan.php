437f07e646f208fe48eb028efa4280b46c7bba0b
robertoDorado <robertodorado7@gmail.com>
2023-07-20 20:00:41 -0300

    model BusinessMan integrado com o modelo de dominio BusinessMan, testes realizados

--- source/Domain/Model/BusinessMan.php
+++ source/Domain/Model/BusinessMan.php
@@ -2,6 +2,9 @@
 
 namespace Source\Domain\Model;
 
+use ReflectionClass;
+use Source\Models\BusinessMan as ModelsBusinessMan;
+
 /**
  * BusinessMan Source\Domain\Model
  * @link 
@@ -30,6 +33,58 @@ class BusinessMan
 
     /** @var bool */
     private bool $validCompany;
+
+    /** @var ModelsBusinessMan Objeto BusinessMan */
+    private ModelsBusinessMan $businessMan;
+
+    public function  setModelBusinessMan(BusinessMan $obj)
+    {
+        $getters = $this->checkGettersFilled($obj);
+        $isMethods = $this->checkIsMethodsFilled($obj);
+
+        if (is_array($getters) && is_array($isMethods)) {
+            $this->businessMan = new ModelsBusinessMan();
+            $this->businessMan->name = $this->getCeoName();
+            $this->businessMan->company_name = $this->getCompanyName();
+            $this->businessMan->company_description = $this->getDescriptionCompany();
+            $this->businessMan->branch_of_company = $this->getBranchOfCompany();
+            $this->businessMan->job_description = $this->getJobDescription();
+            $this->businessMan->valid_company = $this->isValidCompany();
+            if (!$this->businessMan->save()) {
+                throw new \Exception($this->businessMan->fail());
+            }
+        }
+    }
+
+    private function checkIsMethodsFilled(BusinessMan $obj)
+    {
+        $reflection = new ReflectionClass($obj);
+        $isMethods = [];
+
+        foreach ($reflection->getMethods() as $method) {
+            if (strpos($method->name, 'is') === 0 && $method->getNumberOfParameters() === 0) {
+                $propertyName = lcfirst(substr($method->name, 2));
+                $isMethods[$propertyName] = $obj->{$method->name}();
+            }
+        }
+
+        return $isMethods;
+    }
+
+    private function checkGettersFilled(BusinessMan $obj)
+    {
+        $reflection = new ReflectionClass($obj);
+        $getters = [];
+
+        foreach ($reflection->getMethods() as $method) {
+            if (strpos($method->name, 'get') === 0 && $method->getNumberOfParameters() === 0) {
+                $propertyName = lcfirst(substr($method->name, 3));
+                $getters[$propertyName] = $obj->{$method->name}();
+            }
+        }
+
+        return $getters;
+    }
 
     public function setCeoName(string $ceoName)
     {
