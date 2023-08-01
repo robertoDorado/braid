8dd544cf7caff22baf298631b4ab89ff8eaa0ba8
robertoDorado <robertodorado7@gmail.com>
2023-07-31 21:30:30 -0300

    banner completo e responsivo

--- themes/braid-theme/home.php
+++ themes/braid-theme/home.php
@@ -1,7 +1,7 @@
 <?php $v->layout("_theme") ?>
 
 <!-- Header -->
-<header class=" w3-center background-home" style="padding:300px 16px">
+<header class=" w3-center background-home">
     <h1 class="w3-margin" class="first-title"></h1>
     <div class="container-btn">
         <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Quero encontrar freelancers</button>
